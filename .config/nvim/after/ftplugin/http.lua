6dede8cc397948316d52567567f65e0bb14309f3
rizkifuad <rizki.fuad@gmail.com>
2023-07-26 13:44:38 +0700

    add more files

---
+++ .config/nvim/after/ftplugin/http.lua
@@ -0,0 +1,4 @@
+-- Rest.nvim --
+vim.keymap.set('n', '<space>rr', "<Plug>RestNvim<cr>", { silent = true})
+vim.keymap.set('n', '<space>rp', "<Plug>RestNvimPreview<cr>", { silent = true})
+vim.keymap.set('n', '<space>rl', "<Plug>RestNvimLast<cr>", { silent = true})
